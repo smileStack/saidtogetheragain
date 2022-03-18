9543b2938f7da40fd19d117784d3426d0f6b648f
smilestack <ashley.carrasquel@gmail.com>
2022-03-18 01:52:55 -0400

    Typscript create react template

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
+import React from 'react';
 import logo from './logo.svg';
+import { Button } from 'antd';
 import './App.css';
 
 function App() {
@@ -7,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -17,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <Button>Button</Button>
       </header>
     </div>
   );
